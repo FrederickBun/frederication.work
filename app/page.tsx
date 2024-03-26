0edaf0c6bc6e657cce40db45b043e3b580b03ce7
Fengyi Chen <fengyichen126@qq.com>
2024-03-26 21:21:58 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,17 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
+          I'm a front-end developer from{" "}
+          <div
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            China
+          </div>. And I also an OI player from{" "}
+          <div
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            High School Affiliated to Southwest University
+          </div>
         </h2>
       </div>
     </div>
