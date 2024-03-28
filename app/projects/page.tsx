a02a0ad36462729eb56b711373285e734e5fd9f4
Fengyi Chen <fengyichen126@qq.com>
2024-03-28 22:25:34 +0800

    d

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,7 +20,7 @@ export default async function ProjectsPage() {
     return acc;
   }, {} as Record<string, number>);
 
-  const featured = allProjects.find((project) => project.slug === "unkey")!;
+  const featured = allProjects.find((project) => project.slug === "Code-Repo")!;
   const top2 = allProjects.find((project) => project.slug === "planetfall")!;
   const top3 = allProjects.find((project) => project.slug === "highstorm")!;
   const sorted = allProjects
